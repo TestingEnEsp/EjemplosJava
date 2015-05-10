371f99a28b726e77a655934c6e343fbfc1fb14b8
Jose Pablo Sarco <sarcojp@hotmail.com>
2015-05-10 01:16:54 -0300

    Creación de Mi Primer Test

---
+++ src/test/java/com/testingenesp/webdriver/MiPrimerTest.java
@@ -0,0 +1,27 @@
+package com.testingenesp.webdriver;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by TestingEnEsp on 10/05/2015.
+ */
+public class MiPrimerTest {
+
+    @Test
+    public void verificarTituloBlog(){
+        WebDriver driver= new FirefoxDriver();
+
+        driver.navigate().to("http://josepablosarco.wordpress.com");
+
+        Assert.assertTrue("El titulo debe contener Testing", driver.getTitle().contains("Testing"));
+
+        driver.close();
+
+        driver.quit();
+
+
+    }
+}
